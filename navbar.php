c1689fac4d076bbc8dc6ca921ec96858863a4e1d
hazemafara <566508@edu.rocmn.nl>
2023-04-17 10:32:27 +0200

    navbar

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="./css/header.css">
+    <link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
+
+</head>
+
+<body>
+    <ul class="navigation-menu">
+        <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
+        <li><a href="#" class="menu-link">Home</a></li>
+        <li><a href="#" class="menu-link">News</a></li>
+        <li><a href="#" class="menu-link">Contact</a></li>
+        <li><a href="#" class="menu-link">About</a></li>
+    </ul>
+</body>
+
+</html>
