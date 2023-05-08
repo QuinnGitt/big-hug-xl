25febf3d2ad174863c110692625f4ad2ffaeb01d
hazemafara <566508@edu.rocmn.nl>
2023-05-08 10:15:43 +0200

    navbar final update

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
-<?php include "header.php"?>
+<?php include "header.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/homepage.css">
+    <link rel="stylesheet" href="./css/header.css">
     <title>Home</title>
 </head>
+
 <body>
     <div class="home_banner">
         <img src="img/home-banner.png" alt="">
@@ -23,6 +26,7 @@
         <h1>Wat is de Big Hug?</h1>
         <p>De Big Hug is een infrarood warmtedeken met een zit- en rugverwarming en oplaadbare batterij. Kies een van de 3 warmtestanden en geniet urenlang van behaaglijke warmte.</p>
     </div>
-    <?php include "footer.php"?>
+    <?php include "footer.php" ?>
 </body>
+
 </html>
