30178db5329d83afdbf9c4d577b08265977aeea7
hazemafara <566508@edu.rocmn.nl>
2023-05-08 09:41:54 +0200

    navbar aanpassing

--- header.php
+++ header.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <link rel="stylesheet" href="./css/header.css">
-    <link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
-
-</head>
-
-<body>
     <ul class="navigation-menu">
         <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
         <li><a href="#" class="menu-link">Home</a></li>
@@ -15,6 +6,3 @@
         <li><a href="#" class="menu-link">Contact</a></li>
         <li><a href="#" class="menu-link">About</a></li>
     </ul>
-</body>
-
-</html>
