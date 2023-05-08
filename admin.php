3959edd1f5362c8da235f42a639c3251ff8e75db
Adham <337142@student.mboutrecht.nl>
2023-05-08 09:48:50 +0200

    header

--- admin.php
+++ admin.php
@@ -1,2 +1,2 @@
-
+<?php include "header.php"?>
 <?php include "footer.php"?>
--- header.php
+++ header.php
@@ -1,40 +1,11 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./scss/header.css">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <body>
-        <ul class="header">
+<header>
+        <ul class="navigation-menug">
             <li><a href="#"><img src="./img/logo.png" alt="Logo"></a></li>
             <li><a href="#">Home</a></li>
             <li><a href="#">News</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">About</a></li>
         </ul>
-    </body>
-</body>
-
-</html>
-=======
-<header>
-    <a href="index.php"><img class="logo" src="img/logo.png" alt="logo"></a>
-    <nav>
-        <ul class="nav__links">
-            <li><a href="index.php">Big Hug</a></li>
-            <li><a href="aboutUs.php">About Us</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="admin.php">Admin</a></li>
-            <li><a href="faq.php">FAQ</a></li>
-        </ul>
-    </nav>
 </header>
->>>>>>> 9624915f6434f7d37ea0fa996f48d729fd092327
+
+<link rel="stylesheet" href="css/header.css">
