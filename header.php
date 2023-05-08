075f34807cb80697a4e2ae3ee5a2b8d3c4daae25
hazemafara <566508@edu.rocmn.nl>
2023-05-08 09:35:16 +0200

    uyuyuyb

--- header.php
+++ header.php
@@ -1,40 +1,20 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./scss/header.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/header.css">
+    <link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
+
 </head>
 
 <body>
-
-    <body>
-        <ul class="header">
-            <li><a href="#"><img src="./img/logo.png" alt="Logo"></a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">About</a></li>
-        </ul>
-    </body>
+    <ul class="navigation-menu">
+        <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
+        <li><a href="#" class="menu-link">Home</a></li>
+        <li><a href="#" class="menu-link">News</a></li>
+        <li><a href="#" class="menu-link">Contact</a></li>
+        <li><a href="#" class="menu-link">About</a></li>
+    </ul>
 </body>
 
 </html>
-=======
-<header>
-    <a href="index.php"><img class="logo" src="img/logo.png" alt="logo"></a>
-    <nav>
-        <ul class="nav__links">
-            <li><a href="index.php">Big Hug</a></li>
-            <li><a href="aboutUs.php">About Us</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="admin.php">Admin</a></li>
-            <li><a href="faq.php">FAQ</a></li>
-        </ul>
-    </nav>
-</header>
->>>>>>> 9624915f6434f7d37ea0fa996f48d729fd092327
