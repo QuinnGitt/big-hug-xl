50e803670c144186244d18842fe4f1f1f45577c6
hazemafara <566508@edu.rocmn.nl>
2023-05-15 09:10:05 +0200

    navbar links werkend en verwijsen naar paginas

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
+<ul class="navigation-menu">
+    <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
+    <li><a href="#" class="logo-link"><img src="./img/the_big_hug.png" alt="Logo"></a></li>
+    <li><a href="#" class="menu-link">About us </a></li>
+    <li><a href="#" class="menu-link">Contact</a></li>
+    <li><a href="#" class="menu-link">Faq</a></li>
+</ul>
 
-    <ul class="navigation-menu">
-        <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
-        <li><a href="#" class="menu-link">Home</a></li>
-        <li><a href="#" class="menu-link">News</a></li>
-        <li><a href="#" class="menu-link">Contact</a></li>
-        <li><a href="#" class="menu-link">About</a></li>
-    </ul>
-
-    <link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/header.css">
