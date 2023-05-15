0b4bedda20755da82352f9d4cbb7e4473216b8f5
hazemafara <566508@edu.rocmn.nl>
2023-05-15 09:31:29 +0200

    hover effect gefixt op navbar

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <ul class="navigation-menu">
     <li><a href="#" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
-    <li><a href="#" class="logo-link"><img src="./img/the_big_hug.png" alt="Logo"></a></li>
-    <li><a href="#" class="menu-link">About us </a></li>
-    <li><a href="#" class="menu-link">Contact</a></li>
-    <li><a href="#" class="menu-link">Faq</a></li>
+    <li><a href="index.php" class="logo-link"><img src="./img/the_big_hug.png" alt="Logo" style="z-index: -9999"></a></li>
+    <li><a href="aboutUs.php" class="menu-link">About us </a></li>
+    <li><a href="contact.php" class="menu-link">Contact</a></li>
+    <li><a href="faq.php" class="menu-link">Faq</a></li>
 </ul>
 
 <link rel="stylesheet" href="css/header.css">
