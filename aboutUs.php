e5e2dde5d01184cf31ae7accda22207ffc67d0c8
hazemafara <566508@edu.rocmn.nl>
2023-05-16 08:56:49 +0200

    about us pagina af en

--- aboutUs.php
+++ aboutUs.php
@@ -18,7 +18,26 @@
         <img src="img/aboutUs-banner.png">
     </div>
     <div class="title">
-        <h1>aboutUs</h1>
+        <h1>About Us</h1>
+    </div>
+    <div>
+        <p class="aboutUs-divider"><b>Homepage </b> > About us</p>
+    </div>
+    <div>
+        <p class="aboutUs-text">Bighug.com is an innovative online store that specializes in selling high-quality heat cushions designed to provide comfort and relaxation to our valued customers. At Bighug.com, we strongly believe that heat therapy is one of the best ways to calm the body and promote overall wellness. As such, we offer a wide range of heat cushions to suit the unique needs and preferences of our diverse clientele.
+
+            Our heat cushions are carefully crafted using only the finest materials, ensuring optimal durability and functionality. With ease of use and maximum comfort in mind, each cushion is designed to deliver soothing warmth and relaxation to various parts of the body, such as the neck, shoulders, back, and legs.
+
+            At Bighug.com, our mission is to provide our customers with top-of-the-line products that help them relax and reduce their daily stress levels. We are committed to delivering the highest quality heat cushions that are not only effective but also affordable. With our user-friendly website and excellent customer service, shopping at Bighug.com is always a breeze.
+
+            Whether you're looking to unwind after a long day at work, alleviate muscle pain.
+
+
+
+
+
+
+        </p>
     </div>
     <?php include "footer.php" ?>
 </body>
