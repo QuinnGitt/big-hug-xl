4971f6493aa6bfe3dcb103ed9310e660b0d0b6eb
LucasBeemster <117640524+LucasBeemster@users.noreply.github.com>
2023-05-16 09:56:18 +0200

    header links gefixed

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <ul class="navigation-menu">
-    <li><a href="infopage.php" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
-    <li><a href="index.php" class="logo-link"><img src="./img/the_big_hug.png" alt="Logo" style="z-index: -9999"></a></li>
+    <li><a href="index.php" class="logo-link"><img src="./img/logo.png" alt="Logo"></a></li>
+    <li><a href="infopage.php" class="logo-link"><img src="./img/the_big_hug.png" alt="Logo" style="z-index: -9999"></a></li>
     <li><a href="aboutUs.php" class="menu-link">About us </a></li>
     <li><a href="contact.php" class="menu-link">Contact</a></li>
     <li><a href="admin.php" class="menu-link">Admin</a></li>
