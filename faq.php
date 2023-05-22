9f50fd50ae360eccf8031f7834dc13f8b8a1ac74
Quinty <quintydewinter1@gmail.com>
2023-05-22 10:21:20 +0200

    FaQ page is af

--- faq.php
+++ faq.php
@@ -20,27 +20,40 @@
                 </p>
             </div>
         </div>
+        <section>
         <div class="vragen">
             <h2>Algemene vragen en informatie</h2>
-            <h3>Instructievideo's</h3>
-                <p>Van 'how to' filmpjes tot aan het ontstaan en de werking van een warmtekussen.
-                    Onze producten hebben sinds de oprichting al heel wat ontwikkelingen doorgemaakt om onze Big Hug® fans de beste kwaliteit te kunnen bieden. Daarom werken we hard aan nieuwe filmpjes die uitleg kunnen geven over het gebruik en de werking van je Big Hug® warmtekussen of -deken.
-                    De oudere video's laten we staan omdat je daarmee toch alvast op weg geholpen bent. Het kan dus zijn dat je hier en daar wat 'oude' modellen tegenkomt, die inmiddels niet meer beschikbaar zijn. Binnenkort dus nieuwe video's die helemaal up to date zijn. Stay warm!
-                </p>
+            <button class="accordion">Instructievideo's</button>
+                <div class="panel">
+                    <p>
+                        Van 'how to' filmpjes tot aan het ontstaan en de werking van een warmtekussen.
+                        Onze producten hebben sinds de oprichting al heel wat ontwikkelingen doorgemaakt om onze Big Hug® fans de beste kwaliteit te kunnen bieden. Daarom werken we hard aan nieuwe filmpjes die uitleg kunnen geven over het gebruik en de werking van je Big Hug® warmtekussen of -deken.
+                        De oudere video's laten we staan omdat je daarmee toch alvast op weg geholpen bent. Het kan dus zijn dat je hier en daar wat 'oude' modellen tegenkomt, die inmiddels niet meer beschikbaar zijn. Binnenkort dus nieuwe video's die helemaal up to date zijn. Stay warm!
+                    </p>
+                </div>
 
-            <h3>Wat voor technologie gebruikt?</h3>
-                <p>Meer dan alleen warmte
-                    Infrarood heeft meer goede effecten dan alleen een lekker warm gevoel. Naast het verwarmen is bewezen dat infraroodlicht de bloedcirculatie verbetert, pijn verminderd, gewrichtsstijfheid tegengaat, ontstekingen vermindert en de huid revitaliseert.
+            <button class="accordion">Wat voor technologie gebruikt?</button>
+                <div class="panel">
+                    <p>
+                        Meer dan alleen warmte
+                        Infrarood heeft meer goede effecten dan alleen een lekker warm gevoel. Naast het verwarmen is bewezen dat infraroodlicht de bloedcirculatie verbetert, pijn verminderd, gewrichtsstijfheid tegengaat, ontstekingen vermindert en de huid revitaliseert.
+                    </p>
+                </div>
+            <button class="accordion">Is Big Hug® veilig om te gebruiken?</button>
+                <div class="panel">
+                    <p>
+                        Ja, al onze producten zijn veilig om te gebruiken.
+                        Wij nemen jouw veiligheid serieus. Daarom zijn al onze producten uitgerust met een beveiliging tegen oververhitting. De thermostaat zal tussentijds de warmte generatie uitzetten wanneer de daadwerkelijke temperatuur van de Big Hug® te hoog wordt. Dit voorkomt te hoge temperaturen.
+                    </p>
+                </div>
+            <button class="accordion">Wat zijn de verschillen tussen de producten?</button>
+            <div class="panel">
+                <p>
+                    Wil je een verwarmde onderrug en zitgedeelte? Kies dan voor de Big Hug (XL), deze deken verwarmd beide delen en verwarmen dus meer dan een plek! De Big Hug leg je lekker op je stoel om jezelf te verwarmen. De Big Hug XL past goed bij een wat grotere bureaustoel.
                 </p>
-
-            <h3>Is Big Hug® veilig om te gebruiken?</h3>
-                <p>Ja, al onze producten zijn veilig om te gebruiken.
-                    Wij nemen jouw veiligheid serieus. Daarom zijn al onze producten uitgerust met een beveiliging tegen oververhitting. De thermostaat zal tussentijds de warmte generatie uitzetten wanneer de daadwerkelijke temperatuur van de Big Hug® te hoog wordt. Dit voorkomt te hoge temperaturen.
-                </p>
-            <h3>Wat zijn de verschillen tussen de producten?</h3>
-            <p>
-                Wil je een verwarmde onderrug en zitgedeelte? Kies dan voor de Big Hug (XL), deze deken verwarmd beide delen en verwarmen dus meer dan een plek! De Big Hug leg je lekker op je stoel om jezelf te verwarmen. De Big Hug XL past goed bij een wat grotere bureaustoel.
-            </p>
+            </div>
         </div>
+        </section>
+        <script src="js/faq.js"></script>
     </body>
 </html>
